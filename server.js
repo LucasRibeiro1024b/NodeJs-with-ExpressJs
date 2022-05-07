9783f8b59c2d7ebcac3b57c024c9c4f4285ebfbd
RibeiroLucas <lucasribeiro1024b@gmail.com>
2022-05-07 12:45:03 -0300

    work: request, response.

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(request, response) {
+    // console.log(request);
+    response.send("<h1>Hello</h1>");
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
