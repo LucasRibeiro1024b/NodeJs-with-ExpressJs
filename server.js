999597043d7633a8433b4e3a07440a6131bf2d7d
RibeiroLucas <lucasribeiro1024b@gmail.com>
2022-05-07 22:47:44 -0300

    work: routes.

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ app.get("/", function(req, res) {
     res.send("<h1>Hello</h1>");
 });
 
+app.get("/contact", function(req, res){
+    res.send("Contact me at: lucasribeiro1024b@gmail.com");
+});
+
+app.get("/about", function(req, res){
+    let about = "My name is Lucas and I love to learn languages.";
+    res.send(about);
+});
+
+app.get("/hobbies", function(req, res){
+    res.send("<ul><li>Languages</li><li>Jesus</li><li>Computers</li></ul>");
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
